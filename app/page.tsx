86631d8d0360c12da68b6a1192f9dc0360644d06
arpitmoliya <arpitmoliya153@gmail.com>
2025-01-04 00:17:14 +0530

    feat: learn how to do streaming of whole page and specific part of page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { lusitana } from './ui/font';
-import Image from 'next/image';
+import AcmeLogo from "@/app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
+import { lusitana } from "./ui/font";
 
 export default function Page() {
   return (
